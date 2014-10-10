f1a33f50db8cf06c36d111db98d073aeb42eddd9
Lily Seropian <lilyseropian@gmail.com>
2014-10-09 22:26:09 -0400

    Whitelist and blacklist work

--- api.js
+++ api.js
@@ -13,8 +13,8 @@ var TRANSLATIONS = {
  *     otherwise returns the provided name. Note that all official names,
  *     regardless of whether they have shorthand available, will work as
  *     arguments to shouldHave, shouldNotHave, and shouldBeLike.
- * @param  {string} shorthand The name is get the official version of.
- * @return {string} The official version, or shorthand if it is not actually
+ * @param  {String} shorthand The name is get the official version of.
+ * @return {String} The official version, or shorthand if it is not actually
  *     short for anything.
  */
 function shorthandToOfficialName(shorthand) {
@@ -24,10 +24,45 @@ function shorthandToOfficialName(shorthand) {
     return shorthand;
 }
 
+/**
+ * 'Else's have to be handled differently because they're not their own
+ * statement, but a property of an IfStatement.
+ * @param  {Node} AST The AST to search for an else statement.
+ * @return {Boolean} Whether the AST contains an else statement.
+ */
 function hasElse(AST) {
     return acorn.walk.findNodeAt(AST, null, null, function(nodeType, node) {
         return (nodeType === 'IfStatement' && node.alternate !== null);
     }) !== undefined;
+}
+
+/**
+ * Returns shouldHave if shouldHave is true, otherwise performs shouldNotHave
+ * (see their docs for more details).
+ * @param  {String} code The code to check.
+ * @param  {Array.<String>} list The elements to check code for.
+ * @param  {Boolean} shouldHave Whether list is a whitelist or blacklist.
+ * @return {Boolean} Whether code conforms to list.
+ */
+function checkList(code, list, shouldHave) {
+    var AST = acorn.parse(code);
+
+    for (var i = 0; i < list.length; i++) {
+        var eltToFind = shorthandToOfficialName(list[i]);
+        if (eltToFind === 'else') {
+            var foundElse = hasElse(AST);
+            if (foundElse && !shouldHave || !foundElse && shouldHave) {
+                return false;
+            }
+        } else {
+            var foundElt = acorn.walk.findNodeAt(AST, null, null, eltToFind);
+            if (foundElt && !shouldHave || !foundElt && shouldHave) {
+                return false;
+            }
+        }
+    }
+        
+    return true;
 }
 
 /**
@@ -36,40 +71,27 @@ function hasElse(AST) {
  *     'variable declaration'."
  * Only checks for the presence/absence of constructs; does not check for
  *     count (e.g. can't say "This program MUST use 2 'for loop's").
- * @param {string} code The code to check.
- * @param {Array.<string>} whitelist The constructs that must be present in the
+ * @param {String} code The code to check.
+ * @param {Array.<String>} whitelist The constructs that must be present in the
  *     code. Constructs must be valid JavaScript reserved words.
  * @return {boolean} Whether the provided code has the desired functionality.
  */
 function shouldHave(code, whitelist) {
-    var AST = acorn.parse(code);
-
-    for (var i = 0; i < whitelist.length; i++) {
-        var eltToFind = shorthandToOfficialName(whitelist[i]);
-        if (eltToFind === 'else') {
-            if(!hasElse(AST)) {
-                return false;
-            }
-        } else if (!acorn.walk.findNodeAt(AST, null, null, eltToFind)) {
-            return false;
-        }
-    }
-        
-    return true;
+    return checkList(code, whitelist, true);
 }
 
 /**
  * Check whether code does not contain blacklisted functionality.
  * For example, the ability to say "This program MUST NOT use a 'while loop' or
  *     an 'if statement'."
- * @param {string} code The code to check.
- * @param {Array.<string>} blacklist The constructs that must not be present in
+ * @param {String} code The code to check.
+ * @param {Array.<String>} blacklist The constructs that must not be present in
  *     the code. Constructs must be valid JavaScript reserved words.
  * @return {boolean} Whether the provided code does not contain any of the
  *     blacklisted functionalities.
  */
 function shouldNotHave(code, blacklist) {
-    acorn.parse(code);
+    return checkList(code, blacklist, false);
 }
 
 /**
@@ -79,8 +101,8 @@ function shouldNotHave(code, blacklist) {
  * Checks whether sturcture hierarchies exist at any level in the tree; does
  *     not check distance between desired structures (i.e. supports
  *     is-ancestor-of but not is-parent-of).
- * @param {string} code The code to check.
- * @param {Object|string} structure An object represented the desired structure.
+ * @param {String} code The code to check.
+ * @param {Object|String} structure An object represented the desired structure.
  *     Example: {'for': 'if'} checks if there is a 'for loop' that has an
  *         'if' statement somewhere inside it, possible within another
  *         construct. {'for': {'if' : 'while'}} would check for a 'for loop'
