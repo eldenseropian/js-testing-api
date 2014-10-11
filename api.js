b16ac3fc93f76dd942966139e3235398d5931755
Lily Seropian <lilyseropian@gmail.com>
2014-10-11 19:10:56 -0400

    Scoped api

--- api.js
+++ api.js
@@ -1,172 +1,180 @@
-var TRANSLATIONS = {
-    'for': 'ForStatement',
-    'while': 'WhileStatement',
-    'if': 'IfStatement',
-    'break': 'BreakStatement',
-    'continue': 'ContinueStatement',
-    'return': 'ReturnStatement',
-    'function': ['FunctionDeclaration', 'FunctionExpression']
-    // function x() --> FunctionDeclaration
-    // var x = function() --> FunctionExpression
-};
+var checker = (function() {
+    var TRANSLATIONS = {
+        'for': 'ForStatement',
+        'while': 'WhileStatement',
+        'if': 'IfStatement',
+        'break': 'BreakStatement',
+        'continue': 'ContinueStatement',
+        'return': 'ReturnStatement',
+        'function': ['FunctionDeclaration', 'FunctionExpression']
+        // function x() --> FunctionDeclaration
+        // var x = function() --> FunctionExpression
+    };
 
-/**
- * Convience method to allow shorthands instead of official parse statement
- *     names. If the provided name is a shorthand, returns the official version;
- *     otherwise returns the provided name. Note that all official names,
- *     regardless of whether they have shorthand available, will work as
- *     arguments to shouldHave, shouldNotHave, and shouldBeLike.
- * @param  {String} shorthand The name is get the official version of.
- * @return {String} The official version, or shorthand if it is not actually
- *     short for anything.
- */
-function shorthandToOfficialName(shorthand) {
-    if (TRANSLATIONS[shorthand] !== undefined) {
-        return TRANSLATIONS[shorthand];
-    }
-    return shorthand;
-}
-
-/**
- * Find an element in the AST.
- * @param {String|Array.<String>} eltToFind The shorthand or full name of an
- *     element to find, or a list of equivalent element names.
- * @param {Node} AST The code to search.
- * @param {Number?} start  If provided, the location in code to start looking
- *     for eltToFind.
- * @return {Node|undefined} The found element, or undefined if it is not found.
- */
-function findElt(eltToFind, AST, start) {
-    var s = start || null;
-
-    var foundElt = undefined;
-    if (eltToFind === 'else') {
-        foundElt = acorn.walk.findNodeAfter(AST, s, function(nodeType, node) {
-            return (nodeType === 'IfStatement' && node.alternate !== null);
-        });
-    } else if (eltToFind instanceof Array) {
-        foundElt = acorn.walk.findNodeAfter(AST, s, function(nodeType) {
-            return eltToFind.indexOf(nodeType) !== -1;
-        });
-    } else {
-        foundElt = acorn.walk.findNodeAfter(AST, s, eltToFind);
-    }
-    return foundElt;
-}
-
- /**
- * Returns the result of calling shouldHave if the flag shouldHave is true,
- * otherwise returns the result of shouldNotHave. See their docs for more
- *     details.
- * @param  {String|Node} code The code to check.
- * @param  {Array.<String>} list The elements to check code for.
- * @param  {Boolean} shouldHave Whether list is a whitelist or blacklist.
- * @return {Boolean} Whether code conforms to list.
- */
-function checkList(code, list, shouldHave, start) {
-    var s = start || null;
-
-    var AST = code;
-    if (typeof code === 'string') {
-        AST = acorn.parse(code);
-    }
-
-    for (var i = 0; i < list.length; i++) {
-        var eltToFind = shorthandToOfficialName(list[i]);
-        var foundElt = findElt(eltToFind, AST, s);
-        if (foundElt && !shouldHave || !foundElt && shouldHave) {
-            return false;
+    /**
+     * Convience method to allow shorthands instead of official parse statement
+     *     names. If the provided name is a shorthand, returns the official version;
+     *     otherwise returns the provided name. Note that all official names,
+     *     regardless of whether they have shorthand available, will work as
+     *     arguments to shouldHave, shouldNotHave, and shouldBeLike.
+     * @param  {String} shorthand The name is get the official version of.
+     * @return {String} The official version, or shorthand if it is not actually
+     *     short for anything.
+     */
+    function shorthandToOfficialName(shorthand) {
+        if (TRANSLATIONS[shorthand] !== undefined) {
+            return TRANSLATIONS[shorthand];
         }
-    }
-    return true;
-}
-
-/**
- * Check whether code contains whitelisted functionality.
- * For example, the ability to say "This program MUST use a 'for loop' and a
- *     'variable declaration'."
- * Only checks for the presence/absence of constructs; does not check for
- *     count (e.g. can't say "This program MUST use 2 'for loop's").
- * @param {String|Node} code The code to check.
- * @param {Array.<String>} whitelist The constructs that must be present in the
- *     code. Constructs must be valid JavaScript reserved words.
- * @param {Number?} start If provided, the location in code to start looking
- *     for blacklisted functionalities.
- * @return {boolean} Whether the provided code has the desired functionality.
- */
-function shouldHave(code, whitelist, start) {
-    var s = start || null;
-    return checkList(code, whitelist, true, s);
-}
-
-/**
- * Check whether code does not contain blacklisted functionality.
- * For example, the ability to say "This program MUST NOT use a 'while loop' or
- *     an 'if statement'."
- * @param {String|Node} code The code to check.
- * @param {Array.<String>} blacklist The constructs that must not be present in
- *     the code. Constructs must be valid JavaScript reserved words.
- * @param {Number?} start If provided, the location in code to start looking
- *     for blacklisted functionalities.
- * @return {boolean} Whether the provided code does not contain any of the
- *     blacklisted functionalities.
- */
-function shouldNotHave(code, blacklist, start) {
-    var s = start || null;
-    return checkList(code, blacklist, false, s);
-}
-
-/**
- * Check whether code is roughly structured in a specified way.
- * For example, "There should be a 'for loop' and inside of it there should be
- *     an 'if statement'."
- * Checks whether sturcture hierarchies exist at any level in the tree; does
- *     not check distance between desired structures (i.e. supports
- *     is-ancestor-of but not is-parent-of).
- * @param {String} code The code to check.
- * @param {Object|String} structure The structure to find in code.
- *     Example: {'for': 'if'} checks if there is a 'for loop' that has an
- *         'if' statement somewhere inside it, possible within another
- *         construct. {'for': {'if' : 'while'}} would check for a 'for loop'
- *         containing an 'if' statement that contains a 'while' loop. See the
- *         unit tests for more examples. 'if' would check if there is an
- *         'if' statement and is equivalent to doing shouldHave(code, 'if').
- * @return {boolean} Whether the provided code conforms to the desired
- *     structure.
- * TODO: expand so you can use arrays to select siblings, e.g.
- *     {'for': ['if', 'else']}
- */
-function shouldBeLike(code, structure) {
-    var AST = acorn.parse(code);
-    return shouldBeLikeHelper(AST, structure, null);
-}
-
-/**
- * Recursive implementation of shouldBeLike.
- * @param {Node} AST The code to check.
- * @param {Object|String} structure The structure to find in AST.
- * @param {Number?} start The location in AST to start looking for structures.
- * @return {Boolean} Whether the AST conforms to structure.
- */
-function shouldBeLikeHelper(AST, structure, start) {
-    var s = start || null;
-
-    if (typeof structure === 'string') {
-        return shouldHave(AST, [structure], s);
+        return shorthand;
     }
 
-    // Any code matches a structure with no constraints
-    if (Object.keys(structure).length === 0) {
+    /**
+     * Find an element in the AST.
+     * @param {String|Array.<String>} eltToFind The shorthand or full name of an
+     *     element to find, or a list of equivalent element names.
+     * @param {Node} AST The code to search.
+     * @param {Number?} start  If provided, the location in code to start looking
+     *     for eltToFind.
+     * @return {Node|undefined} The found element, or undefined if it is not found.
+     */
+    function findElt(eltToFind, AST, start) {
+        var s = start || null;
+
+        var foundElt = undefined;
+        if (eltToFind === 'else') {
+            foundElt = acorn.walk.findNodeAfter(AST, s, function(nodeType, node) {
+                return (nodeType === 'IfStatement' && node.alternate !== null);
+            });
+        } else if (eltToFind instanceof Array) {
+            foundElt = acorn.walk.findNodeAfter(AST, s, function(nodeType) {
+                return eltToFind.indexOf(nodeType) !== -1;
+            });
+        } else {
+            foundElt = acorn.walk.findNodeAfter(AST, s, eltToFind);
+        }
+        return foundElt;
+    }
+
+     /**
+     * Returns the result of calling shouldHave if the flag shouldHave is true,
+     * otherwise returns the result of shouldNotHave. See their docs for more
+     *     details.
+     * @param  {String|Node} code The code to check.
+     * @param  {Array.<String>} list The elements to check code for.
+     * @param  {Boolean} shouldHave Whether list is a whitelist or blacklist.
+     * @return {Boolean} Whether code conforms to list.
+     */
+    function checkList(code, list, shouldHave, start) {
+        var s = start || null;
+
+        var AST = code;
+        if (typeof code === 'string') {
+            AST = acorn.parse(code);
+        }
+
+        for (var i = 0; i < list.length; i++) {
+            var eltToFind = shorthandToOfficialName(list[i]);
+            var foundElt = findElt(eltToFind, AST, s);
+            if (foundElt && !shouldHave || !foundElt && shouldHave) {
+                return false;
+            }
+        }
         return true;
     }
 
-    var shorthandOfEltToFind = Object.keys(structure)[0];
-    var eltToFind = shorthandToOfficialName(shorthandOfEltToFind);
-    var foundNode = findElt(eltToFind, AST, s);
-    if (foundNode === undefined) {
-        return false;
+    /**
+     * Check whether code contains whitelisted functionality.
+     * For example, the ability to say "This program MUST use a 'for loop' and a
+     *     'variable declaration'."
+     * Only checks for the presence/absence of constructs; does not check for
+     *     count (e.g. can't say "This program MUST use 2 'for loop's").
+     * @param {String|Node} code The code to check.
+     * @param {Array.<String>} whitelist The constructs that must be present in the
+     *     code. Constructs must be valid JavaScript reserved words.
+     * @param {Number?} start If provided, the location in code to start looking
+     *     for blacklisted functionalities.
+     * @return {boolean} Whether the provided code has the desired functionality.
+     */
+    function shouldHave(code, whitelist, start) {
+        var s = start || null;
+        return checkList(code, whitelist, true, s);
     }
 
-    return shouldBeLikeHelper(AST, structure[shorthandOfEltToFind],
-        foundNode.node.start + 1);
-}
+    /**
+     * Check whether code does not contain blacklisted functionality.
+     * For example, the ability to say "This program MUST NOT use a 'while loop' or
+     *     an 'if statement'."
+     * @param {String|Node} code The code to check.
+     * @param {Array.<String>} blacklist The constructs that must not be present in
+     *     the code. Constructs must be valid JavaScript reserved words.
+     * @param {Number?} start If provided, the location in code to start looking
+     *     for blacklisted functionalities.
+     * @return {boolean} Whether the provided code does not contain any of the
+     *     blacklisted functionalities.
+     */
+    function shouldNotHave(code, blacklist, start) {
+        var s = start || null;
+        return checkList(code, blacklist, false, s);
+    }
+
+    /**
+     * Check whether code is roughly structured in a specified way.
+     * For example, "There should be a 'for loop' and inside of it there should be
+     *     an 'if statement'."
+     * Checks whether sturcture hierarchies exist at any level in the tree; does
+     *     not check distance between desired structures (i.e. supports
+     *     is-ancestor-of but not is-parent-of).
+     * @param {String} code The code to check.
+     * @param {Object|String} structure The structure to find in code.
+     *     Example: {'for': 'if'} checks if there is a 'for loop' that has an
+     *         'if' statement somewhere inside it, possible within another
+     *         construct. {'for': {'if' : 'while'}} would check for a 'for loop'
+     *         containing an 'if' statement that contains a 'while' loop. See the
+     *         unit tests for more examples. 'if' would check if there is an
+     *         'if' statement and is equivalent to doing shouldHave(code, 'if').
+     * @return {boolean} Whether the provided code conforms to the desired
+     *     structure.
+     * TODO: expand so you can use arrays to select siblings, e.g.
+     *     {'for': ['if', 'else']}
+     */
+    function shouldBeLike(code, structure) {
+        var AST = acorn.parse(code);
+        return shouldBeLikeHelper(AST, structure, null);
+    }
+
+    /**
+     * Recursive implementation of shouldBeLike.
+     * @param {Node} AST The code to check.
+     * @param {Object|String} structure The structure to find in AST.
+     * @param {Number?} start The location in AST to start looking for structures.
+     * @return {Boolean} Whether the AST conforms to structure.
+     */
+    function shouldBeLikeHelper(AST, structure, start) {
+        var s = start || null;
+
+        if (typeof structure === 'string') {
+            return shouldHave(AST, [structure], s);
+        }
+
+        // Any code matches a structure with no constraints
+        if (Object.keys(structure).length === 0) {
+            return true;
+        }
+
+        var shorthandOfEltToFind = Object.keys(structure)[0];
+        var eltToFind = shorthandToOfficialName(shorthandOfEltToFind);
+        var foundNode = findElt(eltToFind, AST, s);
+        if (foundNode === undefined) {
+            return false;
+        }
+
+        return shouldBeLikeHelper(AST, structure[shorthandOfEltToFind],
+            foundNode.node.start + 1);
+    }
+
+    return {
+        shouldHave: shouldHave,
+        shouldNotHave: shouldNotHave,
+        shouldBeLike: shouldBeLike
+    };
+})();
--- api_tests.js
+++ api_tests.js
@@ -4,7 +4,8 @@ var BLOCK_COMMENT_ONLY = '/** Hello world */';
 var IF = 'var x = 4;\nif (x > 4) {\n\tx++;\n}';
 var IF_ELSE = IF + ' else {\n\tx -= 1;\n}';
 var FOR_LOOP = 'var x = 0;\nfor (var i = 0; i < 5; i++) {\n\tx+=1;\n}';
-var WHILE_LOOP = 'var x = 0;\nwhile(x < 5) {\nfor(var i = 0; i < 2; i++) {\nif (x > i) {\nbreak;\n}\n}\n}';
+var WHILE_LOOP = 'var x = 0;\nwhile(x < 5) {\nfor(var i = 0; i < 2; i++) ' +
+    '{\nif (x > i) {\nbreak;\n}\n}\n}';
 var x = 0;
 var FUNCTION = 'var y = function() {\n' + IF_ELSE + '\n}';
 var FUNCTION_WITH_ARGS = 'function myFunc(y, z) {\n\treturn y + z;\n}';
@@ -27,22 +28,22 @@ var programs = [
 
 QUnit.test('empty whitelist', function(assert) {
     programs.forEach(function(program) {
-        assert.ok(shouldHave(program, []));
+        assert.ok(checker.shouldHave(program, []));
     });
 });
 
 QUnit.test('single whitelist item', function(assert) {
-    // assert.ok(shouldHave(IF, ['if']));
-    // assert.ok(shouldHave(IF_ELSE, ['if']));
-    assert.ok(shouldHave(IF_ELSE, ['else']));
-    // assert.equal(shouldHave(FOR_LOOP, ['if']), false);
+    assert.ok(checker.shouldHave(IF, ['if']));
+    assert.ok(checker.shouldHave(IF_ELSE, ['if']));
+    assert.ok(checker.shouldHave(IF_ELSE, ['else']));
+    assert.equal(checker.shouldHave(FOR_LOOP, ['if']), false);
 });
 
 QUnit.test('multiple whitelist items', function(assert) {
-    assert.ok(shouldHave(IF_ELSE, ['if', 'else']));
-    assert.ok(shouldHave(FUNCTION_WITH_ARGS, ['function', 'return']));
-    assert.equal(shouldHave(IF, ['if', 'else']), false);
-    assert.equal(shouldHave(FOR_LOOP, ['if', 'else']), false);
+    assert.ok(checker.shouldHave(IF_ELSE, ['if', 'else']));
+    assert.ok(checker.shouldHave(FUNCTION_WITH_ARGS, ['function', 'return']));
+    assert.equal(checker.shouldHave(IF, ['if', 'else']), false);
+    assert.equal(checker.shouldHave(FOR_LOOP, ['if', 'else']), false);
 });
 
 /*******************/
@@ -51,24 +52,25 @@ QUnit.test('multiple whitelist items', function(assert) {
 
 QUnit.test('empty blacklist', function(assert) {
     programs.forEach(function(program) {
-        assert.ok(shouldNotHave(program, []));
+        assert.ok(checker.shouldNotHave(program, []));
     });
 });
 
 QUnit.test('single blacklist item', function(assert) {
-    assert.equal(shouldNotHave(IF, ['if']), false);
-    assert.equal(shouldNotHave(IF_ELSE, ['if']), false);
-    assert.equal(shouldNotHave(IF_ELSE, ['else']), false);
-    assert.ok(shouldNotHave(FOR_LOOP, ['if']));
+    assert.equal(checker.shouldNotHave(IF, ['if']), false);
+    assert.equal(checker.shouldNotHave(IF_ELSE, ['if']), false);
+    assert.equal(checker.shouldNotHave(IF_ELSE, ['else']), false);
+    assert.ok(checker.shouldNotHave(FOR_LOOP, ['if']));
 });
 
 QUnit.test('multiple blacklist items', function(assert) {
-    assert.equal(shouldNotHave(IF_ELSE, ['if', 'else']), false);
-    assert.equal(shouldNotHave(IF_ELSE, ['for', 'else']), false);
-    assert.equal(shouldNotHave(IF_ELSE, ['else', 'return']), false);
-    assert.equal(shouldNotHave(FUNCTION_WITH_ARGS, ['function', 'return']), false);
-    assert.equal(shouldNotHave(IF, ['if', 'else']), false);
-    assert.ok(shouldNotHave(FOR_LOOP, ['if', 'else']));
+    assert.equal(checker.shouldNotHave(IF_ELSE, ['if', 'else']), false);
+    assert.equal(checker.shouldNotHave(IF_ELSE, ['for', 'else']), false);
+    assert.equal(checker.shouldNotHave(IF_ELSE, ['else', 'return']), false);
+    assert.equal(checker.shouldNotHave(FUNCTION_WITH_ARGS,
+        ['function', 'return']), false);
+    assert.equal(checker.shouldNotHave(IF, ['if', 'else']), false);
+    assert.ok(checker.shouldNotHave(FOR_LOOP, ['if', 'else']));
 });
 
 /*******************/
@@ -77,21 +79,22 @@ QUnit.test('multiple blacklist items', function(assert) {
 
 QUnit.test('empty structure', function(assert) {
     programs.forEach(function(program) {
-        assert.ok(shouldBeLike(program, {}));
+        assert.ok(checker.shouldBeLike(program, {}));
     });
 });
 
 QUnit.test('single layer structure', function(assert) {
-    assert.ok(shouldBeLike(IF, 'if'));
-    assert.ok(shouldBeLike(IF_ELSE, 'if'));
-    assert.ok(shouldBeLike(IF_ELSE, 'else'));
-    assert.equal(shouldBeLike(FOR_LOOP, 'if'), false);
+    assert.ok(checker.shouldBeLike(IF, 'if'));
+    assert.ok(checker.shouldBeLike(IF_ELSE, 'if'));
+    assert.ok(checker.shouldBeLike(IF_ELSE, 'else'));
+    assert.equal(checker.shouldBeLike(FOR_LOOP, 'if'), false);
 });
 
 QUnit.test('nested structure', function(assert) {
-    assert.ok(shouldBeLike(WHILE_LOOP, {'while': {'for': {'if': 'break'}}}));
-    assert.ok(shouldBeLike(FUNCTION, {'function': 'if'}));
-    assert.ok(shouldBeLike(FUNCTION_WITH_ARGS, {'function': 'return'}));
-    assert.equal(shouldBeLike(IF_ELSE, {'if': 'else'}), false);
-    assert.equal(shouldBeLike(IF_ELSE, {'function': 'else'}), false);
+    assert.ok(checker.shouldBeLike(WHILE_LOOP,
+        {'while': {'for': {'if': 'break'}}}));
+    assert.ok(checker.shouldBeLike(FUNCTION, {'function': 'if'}));
+    assert.ok(checker.shouldBeLike(FUNCTION_WITH_ARGS, {'function': 'return'}));
+    assert.equal(checker.shouldBeLike(IF_ELSE, {'if': 'else'}), false);
+    assert.equal(checker.shouldBeLike(IF_ELSE, {'function': 'else'}), false);
 });
